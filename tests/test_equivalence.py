5c634da012f994132bc9bacf2e057f936b8d1534
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2021-08-19 14:43:34 +0100

    Expand testing to paper over decimal precision issues

--- tests/test_equivalence.py
+++ tests/test_equivalence.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 import pytest
@@ -11,26 +12,56 @@ UFOS = [
 ]
 
 
+def close_enough(a: float, b: float) -> bool:
+    """Determine whether norad's f32 decimals are close enough to Python's
+    f64 decimals."""
+    return math.isclose(a, b, rel_tol=1e-07)
+
+
 @pytest.mark.parametrize("path", UFOS, ids=lambda p: p.name)
 def test_equivalence(path: Path) -> None:
-    ufo_font = ufoLib2.Font.open(path)
-    iondrive_font = iondrive.load(ufoLib2.objects, path)
+    font = ufoLib2.Font.open(path)
+    id_font = iondrive.load(ufoLib2.objects, path)
 
-    assert ufo_font.lib == iondrive_font.lib
-    assert ufo_font.groups == iondrive_font.groups
-    assert ufo_font.kerning == iondrive_font.kerning
-    assert ufo_font.features == iondrive_font.features
-    assert ufo_font.info == iondrive_font.info
-    assert len(ufo_font.layers) == len(iondrive_font.layers)
-    for layer_name in ufo_font.layers.keys():
-        ufo_layer = ufo_font.layers[layer_name]
-        iondrive_layer = iondrive_font.layers[layer_name]
-        assert ufo_layer.color == iondrive_layer.color
-        assert ufo_layer.lib == iondrive_layer.lib
-        assert ufo_layer.keys() == iondrive_layer.keys()
-        for glyph_name in ufo_layer.keys():
-            assert ufo_layer[glyph_name] == iondrive_layer[glyph_name]
+    assert font.lib == id_font.lib
+    assert font.groups == id_font.groups
+    assert font.kerning == id_font.kerning
+    assert font.features == id_font.features
+    assert font.info == id_font.info
+    assert font.layers.keys() == id_font.layers.keys()
+    for layer_name in font.layers.keys():
+        layer = font.layers[layer_name]
+        id_layer = id_font.layers[layer_name]
+        assert layer.color == id_layer.color
+        assert layer.lib == id_layer.lib
+        assert layer.keys() == id_layer.keys()
+        for glyph_name in layer.keys():
+            # Compare fields one by one so we can use approximate equal comparisons.
+            # Expand as necessary.
+            glyph = layer[glyph_name]
+            id_glyph = id_layer[glyph_name]
+            assert glyph.name == id_glyph.name
+            assert close_enough(glyph.width, id_glyph.width)
+            assert close_enough(glyph.height, id_glyph.height)
+            assert glyph.unicodes == id_glyph.unicodes
+            assert glyph.image == id_glyph.image
+            assert glyph.lib == id_glyph.lib
+            assert glyph.note == id_glyph.note
+            assert glyph.anchors == id_glyph.anchors
+            assert glyph.components == id_glyph.components
+            assert len(glyph.contours) == len(id_glyph.contours)
+            for contour, id_contour in zip(glyph.contours, id_glyph.contours):
+                assert contour.identifier == id_contour.identifier
+                assert len(contour.points) == len(id_contour.points)
+                for point, id_point in zip(contour.points, id_contour.points):
+                    assert close_enough(point.x, id_point.x)
+                    assert close_enough(point.y, id_point.y)
+                    assert point.type == id_point.type
+                    assert point.smooth == id_point.smooth
+                    assert point.name == id_point.name
+                    assert point.identifier == id_point.identifier
+            assert glyph.guidelines == id_glyph.guidelines
 
     # Not supported by norad yet:
-    # assert ufo_font.data == iondrive_font.data
-    # assert ufo_font.images == iondrive_font.images
+    # assert font.data == id_font.data
+    # assert font.images == id_font.images
