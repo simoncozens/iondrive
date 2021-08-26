001684a5967bc60fff0e8aa23b239891a19a9b6c
Nikolaus Waxweiler <madigens@gmail.com>
2021-08-26 23:24:04 +0100

    load: leave return type vague
    Not sure how to overload this for ufoLib2.

--- iondrive/__init__.pyi
+++ iondrive/__init__.pyi
@@ -1,8 +1,7 @@
 import os
 from types import ModuleType
-
-import ufoLib2
+from typing import Any
 
 class IondriveError(Exception): ...
 
-def load(font_objects_module: ModuleType, path: str | os.PathLike[str]) -> ufoLib2.Font: ...
+def load(font_objects_module: ModuleType, path: str | os.PathLike[str]) -> Any: ...
